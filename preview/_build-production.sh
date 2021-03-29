2fb00e81b00e8a1516d32ff27a63418f708dadae
gojimmypi <13059545+gojimmypi@users.noreply.github.com>
2021-03-28 17:02:52 -0700

    files added by ickc

---
+++ preview/_build-production.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+python -mwebbrowser http://127.0.0.1:4000/gridster-jekyll-theme/
+JEKYLL_ENV=production bundle exec jekyll serve --profile --drafts --incremental
