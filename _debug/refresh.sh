c80fd3e94d31cab4ffa3b0e59e41519a6b25e2d9
gojimmypi <jim@wolfssl.com>
2022-09-15 13:08:28 -0700

    wolfssl cmd and script now support  param comments

--- _debug/refresh.sh
+++ _debug/refresh.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+# set a variable for the input command
 WOLFSSL_CMD_FILE="/mnt/c/workspace/gojimmypi.github.io/_debug/cmd.txt"
+
+# setup some variables for output files
 WOLFSSL_OUTPUT="/mnt/c/workspace/gojimmypi.github.io/_debug/output.txt"
 WOLFSSL_OPTIONS="/mnt/c/workspace/gojimmypi.github.io/_debug/options.h"
 WOLFSSL_YES="/mnt/c/workspace/gojimmypi.github.io/_debug/Enabled-Features.txt"
 WOLFSSL_NO="/mnt/c/workspace/gojimmypi.github.io/_debug/Disabled-Features.txt"
 
+# show the command text found
 echo "CMD File= $WOLFSSL_CMD_FILE"
 echo ""
 
-WOLFSSL_CMD="$(cat $WOLFSSL_CMD_FILE)"
+# get the contents of the command file, trimming all text after the # character
+WOLFSSL_CMD="$(cat $WOLFSSL_CMD_FILE | cut -d'#' -f1)"
 echo "CMD = $WOLFSSL_CMD"
 echo ""
 
